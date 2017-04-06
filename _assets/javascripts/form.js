17d962e05589edb2382e2a9d1652acf7ad1c603b
Marco Steffensky <marco.steffensky@gmail.com>
2017-04-06 19:34:06 +0000

    Updated '_assets/javascripts/form.js' via CloudCannon

--- _assets/javascripts/form.js
+++ _assets/javascripts/form.js
@@ -1,37 +1,34 @@
-(function() {
+// Helper function to get form data in the supported format
+function getFormDataString(formEl) {
+  var formData = new FormData(formEl),
+      data = [];
 
-  // Helper function to get form data in the supported format
-  function getFormDataString(formEl) {
-    var formData = new FormData(formEl),
-        data = [];
-
-    for (var keyValue of formData) {
-      data.push(encodeURIComponent(keyValue[0]) + "=" + encodeURIComponent(keyValue[1]));
-    }
-
-    return data.join("&");
+  for (var keyValue of formData) {
+    data.push(encodeURIComponent(keyValue[0]) + "=" + encodeURIComponent(keyValue[1]));
   }
 
-  // Fetch the form element
-  var formEl = document.getElementById("contact-form--js");
+  return data.join("&");
+}
 
-  // Override the submit event
-  formEl.addEventListener("submit", function (e) {
-    e.preventDefault();
+// Fetch the form element
+var formEl = document.getElementById("contact-form--js");
 
-    var request = new XMLHttpRequest();
+// Override the submit event
+formEl.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-    request.addEventListener("load", function () {
-      if (request.status === 302) { // CloudCannon redirects on success
-        $("#thank_you").fadeIn();
-        setTimeout(function(){
-          $("#thank_you").fadeOut();
-        }, 3000);
-      }
-    });
+  var request = new XMLHttpRequest();
 
-    request.open(formEl.method, formEl.action);
-    request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    request.send(getFormDataString(formEl));
+  request.addEventListener("load", function () {
+    if (request.status === 302) { // CloudCannon redirects on success
+      $("#thank_you").fadeIn();
+      setTimeout(function(){
+        $("#thank_you").fadeOut();
+      }, 3000);
+    }
   });
+
+  request.open(formEl.method, formEl.action);
+  request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  request.send(getFormDataString(formEl));
 });
