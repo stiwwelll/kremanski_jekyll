31a48e212947e9c0e63d8bbce73d0a7f9420d5a1
Marco <marco.steffensky@gmail.com>
2017-07-12 11:12:21 +0200

    Optimise images

--- _assets/javascripts/form.js
+++ _assets/javascripts/form.js
@@ -1,41 +1,37 @@
-// Helper function to get form data in the supported format
-function getFormDataString(formEl) {
-  var formData = new FormData(formEl),
-      data = [];
+(function() {
 
-  for (var keyValue of formData) {
-    data.push(encodeURIComponent(keyValue[0]) + "=" + encodeURIComponent(keyValue[1]));
+  // Helper function to get form data in the supported format
+  function getFormDataString(formEl) {
+    var formData = new FormData(formEl),
+        data = [];
+
+    for (var keyValue of formData) {
+      data.push(encodeURIComponent(keyValue[0]) + "=" + encodeURIComponent(keyValue[1]));
+    }
+
+    return data.join("&");
   }
 
-  return data.join("&");
-}
+  // Fetch the form element
+  var formEl = document.getElementById("contact-form");
 
-// Fetch the form element
-var formEl = document.getElementById("contact-form");
+  // Override the submit event
+  formEl.addEventListener("submit", function (e) {
+    e.preventDefault();
 
-// Override the submit event
-formEl.addEventListener("submit", function (e) {
-  e.preventDefault();
+    var request = new XMLHttpRequest();
 
-  if (grecaptcha) {
-    var recaptchaResponse = grecaptcha.getResponse();
-    if (!recaptchaResponse) { // reCAPTCHA not clicked yet
-      return false;
-    }
-  }
+    request.addEventListener("load", function () {
+      if (request.status === 302) { // CloudCannon redirects on success
+        $("#thank_you").fadeIn();
+        setTimeout(function(){
+          $("#thank_you").fadeOut();
+        }, 3000);
+      }
+    });
 
-  var request = new XMLHttpRequest();
-
-  request.addEventListener("load", function () {
-    if (request.status === 302) { // CloudCannon redirects on success
-      $("#thank_you").fadeIn();
-      setTimeout(function(){
-        $("#thank_you").fadeOut();
-      }, 3000);
-    }
+    request.open(formEl.method, formEl.action);
+    request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    request.send(getFormDataString(formEl));
   });
-
-  request.open(formEl.method, formEl.action);
-  request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  request.send(getFormDataString(formEl));
 });
